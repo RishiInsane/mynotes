19b7201ea6b4bdf9599ae6dc15ab3ddf4b70928b
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2023-05-05 11:39:31 +0530

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:notes_app/views/login_view.dart';
 import 'package:notes_app/views/register_view.dart';
 import 'package:notes_app/views/verify_email_view.dart';
 import 'firebase_options.dart';
+import 'dart:developer' as devtools show log;
 void main()
 {
   WidgetsFlutterBinding.ensureInitialized();  //initialising firebase.initializeApp
@@ -13,7 +14,7 @@ void main()
       title: 'Flutter Demo',
       theme: ThemeData
         (
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.cyan,
         ),
       home: const HomePage(),
       routes: {
@@ -44,18 +45,75 @@ class _HomePageState extends State<HomePage> {
             final user = FirebaseAuth.instance.currentUser;//to get user
             if(user != null){
               if(user.emailVerified){
-                print('email is verified');
+                return const NotesView();
               }else{
                 return const VerifyEmailView();
               }
             }else{
               return const LoginView();
             }
-            return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
         },
       );
   }
+}
+enum MenuAction{logout}
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [PopupMenuButton<MenuAction>(
+          onSelected:(value) async{
+            switch(value){
+              case MenuAction.logout:
+                final shouldLogut = await showLogOutDialog(context);
+                if(shouldLogut){
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).pushNamedAndRemoveUntil('/login/', (_) => false);
+                }
+                break;
+            }
+        },itemBuilder: (context) {
+          return const [
+            PopupMenuItem<MenuAction>(
+              value: MenuAction.logout,
+              child: Text('Logout'),
+              )
+          ];
+        },)],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context){
+  return showDialog<bool>(
+    context: context,
+    builder:(context) {
+      return AlertDialog(
+        title: const Text('Sign Out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(
+            onPressed: () {Navigator.of(context).pop(false);},//pops the alert dialog box based on the value of true/false
+            child: const Text('Cancel')),
+            TextButton(
+            onPressed: () {Navigator.of(context).pop(true);},
+            child: const Text('Logout')),
+        ],
+      );
+    },).then((value) => value ?? false); //if the dialog gets dismissed the future function gets completed without getting any value then the .then() function helps us to either get some value or is false by default 
 }
