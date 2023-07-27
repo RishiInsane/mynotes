257948ca6182cd989ae913e8d470502661604207
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2023-07-27 19:51:45 +0530

    step-27

---
+++ lib/utilities/dialogs/password_reset_email_sent_dialog.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:notes_app/utilities/dialogs/generic_dialog.dart';
+
+Future<void> showPasswordResetSentDialog(BuildContext context) {
+  return showGenericDialog<void>(
+    context: context,
+    title: 'Password Reset',
+    content:
+        'Password reset link has been sent to your email. Please check for more info..',
+    optionsBuilder: () => {
+      'OK': null,
+    },
+  );
+}
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -127,4 +127,9 @@ class MockAuthProvider implements AuthProvider {
     );
     _user = newUser;
   }
+
+  @override
+  Future<void> sendPasswordReset({required String toEmail}) {
+    throw UnimplementedError();
+  }
 }
